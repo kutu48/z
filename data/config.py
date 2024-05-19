a58313f87143a063ef8f695b17f3790001757a32
Kutujaya <77893381+kutu48@users.noreply.github.com>
2024-05-19 18:20:21 +0700

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,15 @@
+# api id, hash
+API_ID = 11111111
+API_HASH = 'Your api hash'
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [1, 15]
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = True # True/False
+
+# полный путь к вашей папке sessions 
+SESSIONS_PATH = "YOUR PATH TO SESSIONS"
