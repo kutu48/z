019fc1acb506ed3eef3813f22ad701364c7ef00c
Kutujaya <77893381+kutu48@users.noreply.github.com>
2024-05-19 18:39:00 +0700

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -1,6 +1,1 @@
-from pyrogram import Client
-from data.config import API_ID, API_HASH, SESSIONS_PATH
-
-def send_message(session_name, chat_id, message):
-    with Client(session_name, api_id=API_ID, api_hash=API_HASH, workdir=SESSIONS_PATH) as app:
-        app.send_message(chat_id, message)
+# telegram.py
