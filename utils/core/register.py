c4207215b48ffcbe224e9521f2ada314b44cbfe3
Kutujaya <77893381+kutu48@users.noreply.github.com>
2024-05-19 18:37:10 +0700

    Update register.py

--- utils/core/register.py
+++ utils/core/register.py
@@ -1,7 +1,1 @@
-from pyrogram import Client
-from data.config import API_ID, API_HASH, WORKDIR
-
-def register_session(session_name):
-    app = Client(session_name, api_id=API_ID, api_hash=API_HASH, workdir=WORKDIR)
-    app.start()
-    app.stop()
+# register.py
